b7bf051a98ae5de357f0f9a868909aa8e79929e7
Ferdy <ferdian.aditya2302@gmail.com>
2019-01-07 10:31:56 +0700

    update 07/01/2019

--- Project/HalamanLogin/partials/footer.php
+++
@@ -1,4 +0,0 @@
-
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script type="text/javascript" src="HalamanLogin/assets/vendor.js"></script>
-	<script type="text/javascript" src="HalamanLogin/assets/bundle.js"></script>
---
+++ dashboard.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Dashboard</title>
+</head>
+<body>
+	<h1>Selamat Datang Dihalaman Admin</h1>
+</body>
+</html>
