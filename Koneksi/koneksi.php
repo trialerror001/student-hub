8ec08360f9fca80ecebca228ebac0e57f71063e2
Ferdy <ferdian.aditya2302@gmail.com>
2019-01-07 11:54:39 +0700

    File koneksi ke database

---
+++ Koneksi/koneksi.php
@@ -0,0 +1,21 @@
+<?php
+	/*$myHost = "mysql.idhostinger.com";
+	$myUser = "u284822025_root";
+	$myPass = "ferbynia";
+	$myDbs = "u284822025_st";*/
+        
+    $myHost = "localhost";
+	$myUser = "root";
+	$myPass = "";
+	$myDbs = "sistem_ruangan";
+	
+	
+	$koneksidb = @mysql_connect($myHost, $myUser, $myPass) ;
+	
+	if (!$koneksidb)
+	{
+		echo " Failed Connection";
+	}
+	mysql_select_db($myDbs) or die ("Database not Found");
+	
+?>
