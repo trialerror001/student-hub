5af621f01f9240398df66b150beb70ea7bcdeef2
Xenozare <zerodavin@gmail.com>
2019-01-08 14:13:45 +0700

    file assets dan partials di restore karena itu berisi bootstrap dan jquery penting

---
+++ partials/footer.php
@@ -0,0 +1,4 @@
+
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/vendor.js"></script>
+	<script type="text/javascript" src="assets/bundle.js"></script>
