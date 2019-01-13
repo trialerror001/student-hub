fe1218aa81f2bcf01724d8d988f933ea6da9df78
Ferdy <ferdian.aditya2302@gmail.com>
2019-01-13 09:42:05 +0700

    Pembaharuan file di HalamanLogin [13/01/2019]

--- HalamanLogin/partials/admin_navbar.php
+++ HalamanLogin/partials/admin_navbar.php
@@ -1,8 +1,5 @@
 <div class="header navbar" style="background-color:#FF6918; width:100%;">
 	<div class="header-container"> 
-		<ul class="nav-left">
-			<img src="HalamanLogin/assets/static/images/logo.png" height="1.em" width="2.em">
-		</ul>
 		<ul class="nav-right">
 			<li class="dropdown">
 				<a href="" class="dropdown-toggle no-after peers fxw-nw ai-c lh-1" data-toggle="dropdown">
@@ -19,10 +16,18 @@
 					<li>
 						<a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
 							<i class="ti-power-off mR-10"></i> 
+							<span>Penambahan ruangan</span>
+						</a>
+					</li>
+				<li role="separator" class="divider"></li>
+					<li>
+						<a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+							<i class="ti-power-off mR-10"></i> 
 							<span>Logout</span>
 						</a>
 					</li>
 				</ul>
+
 			</li>
 		</ul>
 	</div>
