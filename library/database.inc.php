1651e9f7435097403691766ddfcfb70e091ee203
Ferdy <ferdian.aditya2302@gmail.com>
2019-01-14 14:32:59 +0700

    Silahkan untuk password koneksi menyesuaikan dengan settingan phpmyadmin masing-masing

--- library/database.inc.php
+++ library/database.inc.php
@@ -2,7 +2,8 @@
 
 $dbServername = "localhost";
 $dbUsername = "root";
-$dbPassword = "root";
+//$dbPassword = "root";
+$dbPassword = "";
 $dbName = "sistem_ruangan";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
