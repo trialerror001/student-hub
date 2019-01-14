acf39983305fca251197f305a4ca8094d5aa8376
Ferdy <ferdian.aditya2302@gmail.com>
2019-01-14 13:45:56 +0700

    Perbaikan file page.php [14/01/2019]

--- Page/page.php
+++ Page/page.php
@@ -8,11 +8,35 @@ if ($_GET) {
                 die("Sorry Empty Page!");
             include "validasi.php";
             break;
+
+        case 'FormRegistrasi' :
+            if (!file_exists("form_regis.php"))
+                die("Sorry Empty Page!");
+            include "form_regis.php";
+            break;
+
+        case 'HalamanAdmin' :
+            if (!file_exists("client_home.php"))
+                die("Sorry Empty Page!");
+            include "client_home.php";
+            break;
+
+        case 'LoginAdmin' :
+            if (!file_exists("admin/index.php"))
+                die("Sorry Empty Page!");
+            include "admin/index.php";
+            break;
+
+
+        default:
+            if (!file_exists("client_home.php"))
+                die("Sorry Empty Page!");
+            include "client_home.php";
     }
 } else {
-    if (!file_exists("HalamanLogin/login.php"))
+    if (!file_exists("client_home.php"))
         die("Empty Main Page!");
     
-    include "HalamanLogin/login.php";
+    include "client_home.php";
 }
 ?>
