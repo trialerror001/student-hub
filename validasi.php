fcd5ae2230de64682e78dd460dbebb5c8d150a56
Ferdy <ferdian.aditya2302@gmail.com>
2019-01-14 13:49:06 +0700

    Perbaikan file validasi untuk login ke dalam halaman admin [14/01/2019]

--- validasi.php
+++ validasi.php
@@ -1,72 +1,35 @@
 <?php
-
-session_start();
-
+//session_start();
 if (isset($_POST['btnLogin'])) {
 
     include 'library/database.inc.php';
 
-    $user = mysqli_real_escape_string($conn, $_POST['email']);
+    $user = mysqli_real_escape_string($conn, $_POST['user']);
     $pass = mysqli_real_escape_string($conn, $_POST['pwd']);
 
     //cek user
-    $sql = "SELECT * FROM tb_login WHERE username='$user'";
+    $sql = "SELECT * FROM tb_login WHERE username='".$user."' and password = '".md5($pass)."'";
     $result = mysqli_query($conn, $sql);
-		$resultCheck = mysqli_num_rows($result);
+		if(mysqli_num_rows($result)){
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION['level'] = $row['level'];
 
+            if ($_SESSION['level'] == "admin") {
+                ?>
+                <script>
+                   document.location = "?page=HalamanAdmin";
+                </script>
+    <?php
 
-		if ($resultCheck < 1) {
-				header ("Location: ../student-hub/index.php?login=error");
-				exit();
-		} else {
-
-        if ($row = mysqli_fetch_assoc($result)) {
-        //Cek Password
-        $pwdCheck = password_verify($pass, $row['password']);
-            if ($pwdCheck == false) {
-                header ("Location: ../student-hub/index.php?login=error");
-                exit();
-            } elseif ($pwdCheck == true) {
-                $_SESSION['email'] = $row['username'];
-                $_SESSION['password'] = $row['password'];
-                $_SESSION['level'] = $row['level'];
-            		if ($_SESSION['level'] == "admin") {
-                		?>
-                		<script>
-                        <?php
-                            header("Location: ../student-hub/reservasi_ruangan.php?login=admin");
-                            exit();
-                         ?>
-                		</script>
-                		<?php
-
-            		} elseif ($_SESSION['level'] == "bidang1" || $_SESSION['level'] == "bidang2" || $_SESSION['level'] == "bidang3" ||
-                    		$_SESSION['level'] == "bidang4" || $_SESSION['level'] == "bidang5") {
-                		?>
-                		<script>
-                    		document.location = "?page=Data-Bidang";
-                		</script>
-                		<?php
-
-            		}elseif($_SESSION['level']=="pegawai"){
-                		?>
-                 		<script>
-                    <?php
-                        header("Location: ../student-hub/index.php?login=success");
-                        exit();
-                     ?>
-                		</script>
-                		<?php
-            		}
-        		} else {
-            		?>
-            		<script>
-                		alert('Username atau Password Anda Salah')
-                		document.location = "index.php";
-            		</script>
-            		<?php
             }
-          }
-    }
+        }else {
+                    ?>
+                    <script>
+                        alert('Username atau Password Anda Salah')
+                        document.location = "?page=LoginAdmin";
+                    </script>
+                    <?php
+            }
+  
 }
 ?>
